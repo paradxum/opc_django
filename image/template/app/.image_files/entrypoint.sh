467adeea56f5bbea2c4237d8315e76d261610c7b
Eric Schultz <eric.schultz@cyvon.com>
2019-04-17 12:46:02 -0500

    Bitnami postgres does a start/stop thing when setting up, need to account for that.

--- image/template/app/.image_files/entrypoint.sh
+++ image/template/app/.image_files/entrypoint.sh
@@ -5,6 +5,11 @@ if [ "$SQL_ENGINE" = "django.db.backends.postgresql" ]; then
 	while ! nc -z $SQL_HOST $SQL_PORT ; do
 		sleep 0.1
 		done
+	# Bitnami postgres likes to start, setup, stop then start again... so give it a chance to do that.
+	sleep 3
+	while ! nc -z $SQL_HOST $SQL_PORT ; do
+		sleep 0.1
+		done
 	echo "PostgreSQL started"
 	fi
 
