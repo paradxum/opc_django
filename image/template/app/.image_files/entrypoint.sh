effbda2b3cad030792062b639591a54231ea7230
Eric Schultz <eric.schultz@cyvon.com>
2022-07-14 13:21:18 -0500

    Rollup updates for new Bitnami Postgress and Redis charts

--- image/template/app/.image_files/entrypoint.sh
+++ image/template/app/.image_files/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 if [ "$SQL_ENGINE" = "django.db.backends.postgresql" ]; then
-	echo "Waiting for postgres..."
-	while ! nc -z $SQL_HOST $SQL_PORT ; do
-		sleep 0.1
-		done
-	# Bitnami postgres likes to start, setup, stop then start again... so give it a chance to do that.
-	sleep 3
-	while ! nc -z $SQL_HOST $SQL_PORT ; do
-		sleep 0.1
-		done
-	echo "PostgreSQL started"
-	fi
+    echo "Waiting for postgres..."
+    while ! nc -z $SQL_HOST $SQL_PORT ; do
+        sleep 0.1
+        done
+    # Bitnami postgres likes to start, setup, stop then start again... so give it a chance to do that.
+    sleep 3
+    while ! nc -z $SQL_HOST $SQL_PORT ; do
+        sleep 0.1
+        done
+    echo "PostgreSQL started"
+    fi
 
-python$$PYTHON_VER$$ manage.py migrate --noinput
-python$$PYTHON_VER$$ manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
 exec "$@"
